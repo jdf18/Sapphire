7b5d261cc8bb31b72389b1776fb7ad8950775465
jdf18 <97624626+jdf18@users.noreply.github.com>
2024-06-03 22:53:02 +0100

    Fixing the btree because of multiple definitions error

--- include/btree.h
+++ include/btree.h
@@ -1,10 +1,11 @@
 #ifndef SAPPHIRE_BTREE_H
 #define SAPPHIRE_BTREE_H
 
+#include "definitions.h"
+
 #include <stdint.h>
 
 #define ROW_SIZE 0
-#define PAGE_SIZE 4096
 
 typedef uint8_t NodeType;
 enum {
@@ -20,18 +21,16 @@ typedef struct {
     uint8_t parent_pointer;
 } NodeHeader;
 
-// ((NodeHeader*)0)->node_type is used because -> needs to be used on an instance.
-//   ((NodeHeader*)0) represents a NodeHeader at address 0
-const uint32_t NODE_TYPE_SIZE = sizeof(((NodeHeader*)0)->node_type);
-const uint32_t NODE_TYPE_OFFSET = offsetof(NodeHeader, node_type);
+extern const uint32_t NODE_TYPE_SIZE;
+extern const uint32_t NODE_TYPE_OFFSET;
 
-const uint32_t NODE_IS_ROOT_SIZE = sizeof(((NodeHeader*)0)->is_root);
-const uint32_t NODE_IS_ROOT_OFFSET = offsetof(NodeHeader, is_root);
+extern const uint32_t NODE_IS_ROOT_SIZE;
+extern const uint32_t NODE_IS_ROOT_OFFSET;
 
-const uint32_t NODE_PARENT_POINTER_SIZE = sizeof(((NodeHeader*)0)->parent_pointer);
-const uint32_t NODE_PARENT_POINTER_OFFSET = offsetof(NodeHeader, parent_pointer);
+extern const uint32_t NODE_PARENT_POINTER_SIZE;
+extern const uint32_t NODE_PARENT_POINTER_OFFSET;
 
-const uint32_t NODE_HEADER_SIZE = sizeof(NodeHeader);
+extern const uint32_t NODE_HEADER_SIZE;
 
 
 // LeafNodeHeader struct, sizes, and offsets
@@ -39,12 +38,12 @@ typedef struct {
     uint32_t num_cells;
 } LeafNodeHeader;
 
-const uint32_t LEAF_NODE_NUM_CELLS_SIZE = sizeof(((LeafNodeHeader*)0)->num_cells);
-const uint32_t LEAF_NODE_NUM_CELLS_OFFSET = offsetof(LeafNodeHeader, num_cells);
-const uint32_t LEAF_NODE_NUM_CELLS_HEADER_OFFSET = NODE_HEADER_SIZE + offsetof(LeafNodeHeader, num_cells);
+extern const uint32_t LEAF_NODE_NUM_CELLS_SIZE;
+extern const uint32_t LEAF_NODE_NUM_CELLS_OFFSET;
+extern const uint32_t LEAF_NODE_NUM_CELLS_HEADER_OFFSET;
 
-const uint32_t LEAF_NODE_HEADER_SIZE = LEAF_NODE_NUM_CELLS_SIZE;
-const uint32_t LEAF_NODE_FULL_HEADER_SIZE = NODE_HEADER_SIZE + LEAF_NODE_HEADER_SIZE;
+extern const uint32_t LEAF_NODE_HEADER_SIZE;
+extern const uint32_t LEAF_NODE_FULL_HEADER_SIZE;
 
 
 // LeafNodeCell struct, sizes, and offsets
@@ -53,13 +52,13 @@ typedef struct {
     uint8_t value[];
 } LeafNodeCell;
 
-const uint32_t LEAF_NODE_KEY_SIZE = sizeof(uint32_t);
-const uint32_t LEAF_NODE_KEY_OFFSET = 0;
+extern const uint32_t LEAF_NODE_KEY_SIZE;
+extern const uint32_t LEAF_NODE_KEY_OFFSET;
 
-const uint32_t LEAF_NODE_VALUE_SIZE = ROW_SIZE;
-const uint32_t LEAF_NODE_VALUE_OFFSET = LEAF_NODE_KEY_OFFSET + LEAF_NODE_KEY_SIZE;
+extern const uint32_t LEAF_NODE_VALUE_SIZE;
+extern const uint32_t LEAF_NODE_VALUE_OFFSET;
 
-const uint32_t LEAF_NODE_CELL_SIZE = LEAF_NODE_KEY_SIZE + LEAF_NODE_VALUE_SIZE;
+extern const uint32_t LEAF_NODE_CELL_SIZE;
 
 
 // LeafNode struct, sizes, and offsets
@@ -69,8 +68,15 @@ typedef struct {
     LeafNodeCell cells[];
 } LeafNode;
 
-const uint32_t LEAF_NODE_CELL_SPACE = PAGE_SIZE - LEAF_NODE_FULL_HEADER_SIZE;
+extern const uint32_t LEAF_NODE_CELL_SPACE;
+extern const uint32_t LEAF_NODE_MAX_CELLS;
 
-const uint32_t LEAF_NODE_MAX_CELLS = LEAF_NODE_CELL_SPACE / LEAF_NODE_CELL_SIZE;
+
+typedef struct {
+    NodeType node_type;
+    union {
+        LeafNode* leaf_node;
+    };
+} Node;
 
 #endif //SAPPHIRE_BTREE_H
